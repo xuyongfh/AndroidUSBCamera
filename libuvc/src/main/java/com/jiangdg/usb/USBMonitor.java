84ca1ba9ca52bc15c6da7a4aa89d04d732549e6b
zhicheng.liu <zhicheng.liu@beautydata.ai>
2024-01-19 16:44:18 +0800

    feat:兼容android14

--- libuvc/src/main/java/com/jiangdg/usb/USBMonitor.java
+++ libuvc/src/main/java/com/jiangdg/usb/USBMonitor.java
@@ -177,8 +177,7 @@ public final class USBMonitor {
 					// avoid acquiring intent data failed in receiver on Android12
 					// when using PendingIntent.FLAG_IMMUTABLE
 					// because it means Intent can't be modified anywhere -- jiangdg/20220929
-					int PENDING_FLAG_IMMUTABLE = 1<<25;
-					mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), PENDING_FLAG_IMMUTABLE);
+					mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
 				} else {
 					mPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
 				}
@@ -187,6 +186,11 @@ public final class USBMonitor {
 				filter.addAction(ACTION_USB_DEVICE_ATTACHED);
 				filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
 				context.registerReceiver(mUsbReceiver, filter);
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+					context.registerReceiver(mUsbReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
+				} else {
+					context.registerReceiver(mUsbReceiver, filter);
+				}
 			}
 			// start connection check
 			mDeviceCounts = 0;
